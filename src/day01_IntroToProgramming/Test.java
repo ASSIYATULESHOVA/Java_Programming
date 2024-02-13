22156e290c1f2f2301e5062f00e1469b6853a6fb
AsiyaTuleshova <assiyanyckz@gmail.com>
2024-02-12 20:28:53 -0500

    Initial commit

---
+++ src/day01_IntroToProgramming/Test.java
@@ -0,0 +1,6 @@
+package day01_IntroToProgramming;
+
+public class Test {
+
+    String word = "Wooden Spoon";
+}
---
+++ src/day02_EscapeSequences/AboutMe.java
@@ -0,0 +1,12 @@
+package day02_EscapeSequences;
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+
+        System.out.println("\tMy favorite book is: \"Eat Pray Love\"\n\tMy favorite quote is: \"That's what she said\"" +
+                "\n\tMy favorite TV show is: \"The office\"\n\tMy favorite music genre is: HipHop");
+
+
+    }
+}
