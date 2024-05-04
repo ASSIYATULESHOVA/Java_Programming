a1f7d39abd156b5dbfb5275a3d0b730c6d892d92
Asiya <149961368+ASSIYATULESHOVA@users.noreply.github.com>
2024-05-03 20:55:16 -0400

    My class 1

--- src/day13_/ReturnMethodPractice.java
+++ src/day13_/ReturnMethodPractice.java
@@ -2,4 +2,56 @@ package day13_;
 
 public class ReturnMethodPractice {
 
+    public static void main(String[] args) {
+
+        System.out.println(  isOdd(100)  );
+
+        int num = 200;
+
+        if( isEven(num) ){
+            System.out.println(num +" is even number");
+        }else{
+            System.out.println(num);
+        }
+
+
+        System.out.println("-------------------------------------");
+
+
+
+    }
+
+
+    public static boolean isOdd(int num){
+        // return (num % 2 != 0)? true : false;
+        if(num %2 != 0){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public static boolean isEven(int num){
+        return !isOdd(num);
+        //  return  (num % 2 == 0)? true : false;
+    }
+
+
+    public static int max(int n1, int n2){
+        // return  n1 > n2 ? n1 : n2;
+
+        if(n1 > n2){
+            return  n1;
+        }else{
+            return n2;
+        }
+
+    }
+
+
+
 }
+
+
+
